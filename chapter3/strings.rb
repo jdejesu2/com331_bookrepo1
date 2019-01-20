89ab3a8a7d2d811beaf9f571c78bc396a62d707d
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-01-20 13:00:02 -0600

    problems aka exercises for chapter 3

---
+++ chapter3/strings.rb
@@ -0,0 +1,53 @@
+puts 'Hello, world!'
+puts ' '
+puts 'Good-bye.'
+##OUTPUTS
+##
+##Hello, world!
+##Good-bye.
+
+
+puts 'I like'+'apple pie'
+
+puts 'I like '+'apple pie'
+puts 'I like' + ' apple pie'
+##OUTPUTS
+##I likeapple pie
+##I like apple pie
+##I like apple pie
+
+
+puts 'blink ' * 4
+##OUTPUTS
+##blink blink blink blink
+
+
+puts  12  +  12
+puts '12' + '12'
+puts '12  +  12'
+##OUTPUTS
+##24
+##1212
+##12  +  12
+
+
+puts  2  *  5
+puts '2' *  5
+puts '2  *  5'
+##OUTPUTS
+##
+
+##OUTPUTS
+##
+
+##OUTPUTS
+##
+
+##OUTPUTS
+##
+
+##OUTPUTS
+##
+
+##OUTPUTS
+##
---
+++ chapter3/swell.rb
@@ -0,0 +1,12 @@
+
+puts puts 'You\'re swell!'
+puts 'backslash at the end of a string:  \\'
+puts 'up\\down'
+puts 'up\down'
+
+##OUTPUTS
+##You're swell!
+##(space?)
+##backslash at the end of a string:  \
+##up\down
+##up\down
