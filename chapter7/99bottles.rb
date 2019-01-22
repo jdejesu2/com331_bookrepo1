0ba25609fcc5cf58978ed9961d41d945b211c454
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-01-21 22:48:36 -0600

    exercise of bottles of beer on the wall

--- chapter7/99bottles.rb
+++ chapter7/99bottles.rb
@@ -1,11 +1,10 @@
 number = 99
 while number > 0
-  puts number.to_s + ' bottles on the wall'
-  puts number.to_s + ' bottles on the wall'
-    if number > 0
-      puts 'take one down, pass it around ' + number.to_s + ' bottles of beer on the wall'
-    else number = 0
-      puts 'take one down, pass it around. No bottles of beer on the wall'
-  end
+  puts number.to_s + ' bottles of beer on the wall'
+  puts number.to_s + ' bottles of beer'
   number = number - 1
+    if number > 1
+      puts 'take one down, pass it around ' + number.to_s + ' bottles of beer on the wall'
+  end
+  puts 'take one down, pass it around. No bottles of beer on the wall'
 end
