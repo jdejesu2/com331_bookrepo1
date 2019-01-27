5622e5542c881c5002cecd3cfb6348c7d00a0a9a
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-01-27 16:18:50 -0600

    working on chapter 12

---
+++ chapter12/alphaarray.rb
@@ -0,0 +1,12 @@
+alpha = Array.new  + [12345]  #  Array addition.
+beta  = String.new + 'hello'  #  String addition.
+karma = Time.new              #  Current date and time.
+puts "alpha = #{alpha}"
+puts "beta  = #{beta}"
+puts "karma = #{karma}"
+
+alpha = [12345]
+
+beta = hello
+
+karma = 2013-06-15 22:59:02 -0700
---
+++ chapter12/timeclass.rb
@@ -0,0 +1,12 @@
+time  = Time.new   #  The moment we ran this code.
+time2 = time + 60  #  One minute later.
+puts time
+puts time2
+
+##2019-01-27 16:17:31 -0600
+##2019-01-27 16:18:31 -0600
+
+puts Time.local(2000, 1, 1)          #  Y2K.
+puts Time.local(1976, 8, 3, 13, 31)  #  When I was born.
+##2000-01-01 00:00:00 -0600
+##1976-08-03 13:31:00 -0500
