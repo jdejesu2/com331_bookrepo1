6401d32efe1513307044cb6c6e5ee7ab33bde8a6
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-01-28 14:50:20 -0600

    chapter 15 notes

---
+++ chapter15/examples.rb
@@ -0,0 +1,15 @@
+##tim today
+##puts 'combergearl thememberate' if 5 == 2**2 + 1**1
+##puts 'supposine follutify' unless 'Chris'.length == 5
+##combergearl thememberate
+
+##block.call
+##def do_it_twice
+  ##yield
+  ##yield
+##end
+##do_it_twice do
+  ##puts 'buritiate mustripe lablic acticise'
+##end
+##buritiate mustripe lablic acticise
+##buritiate mustripe lablic acticise
